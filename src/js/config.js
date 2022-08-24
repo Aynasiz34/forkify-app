effe9596b16a2990c2b4dfb14261ad878349ee1a
Aynasiz34 <gomserim@gmail.com>
2022-08-24 17:36:02 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd4b83e0a-2284-4c61-9f11-a5f44d478801';
+export const MODAL_CLOSE_SECONDS = 2.5;
